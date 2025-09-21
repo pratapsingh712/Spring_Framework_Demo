ca4482bf68948401570a33396e3ac091c2389f20
Raghvendra Pratap Singh <rpsingh09mar@gmail.com>
2025-09-21 15:13:26 +0530

    added java based config

--- src/main/java/org/raghav/App.java
+++ src/main/java/org/raghav/App.java
@@ -1,6 +1,8 @@
 package org.raghav;
 
+import org.raghav.config.AppConfig;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
@@ -8,17 +10,21 @@ public class App
 {
     public static void main( String[] args )
     {
-//        System.out.println( "Hello World!" );
 
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml"); // this line create the container for me and applicationContext is an interface
-        Alien obj = applicationContext.getBean("alien",Alien.class);
-//        obj.setAge(25); using this i am trying to override the property already set for age in the spring.xml
-        System.out.println(obj.getAge());
-        obj.code();
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        Desktop obj1 = applicationContext.getBean("com1",Desktop.class);
+        Desktop dt = context.getBean(Desktop.class);
+        dt.compile();
 
-        Computer com = applicationContext.getBean(Computer.class); // we are able to create bean for interface also because when you load the class interface class also get's loaded
+//        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml"); // this line create the container for me and applicationContext is an interface
+//        Alien obj = applicationContext.getBean("alien",Alien.class);
+////        obj.setAge(25); using this i am trying to override the property already set for age in the spring.xml
+//        System.out.println(obj.getAge());
+//        obj.code();
+//
+//        Desktop obj1 = applicationContext.getBean("com1",Desktop.class);
+//
+//        Computer com = applicationContext.getBean(Computer.class); // we are able to create bean for interface also because when you load the class interface class also get's loaded
 
     }
 }
--- src/main/java/org/raghav/config/AppConfig.java
+++ src/main/java/org/raghav/config/AppConfig.java
@@ -1,4 +1,13 @@
 package org.raghav.config;
 
+import org.raghav.Desktop;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class AppConfig {
+    @Bean
+    public Desktop desktop(){
+        return new Desktop();
+    }
 }
